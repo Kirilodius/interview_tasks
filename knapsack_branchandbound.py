cb29bd1ac60459f6b9cea8dfe0cb33d442c3cad9
Kirill <kirilodius@gmail.com>
2015-08-12 00:36:15 +0200

    Update knapsack_branchandbound.py
    + calcBound needs to refine current bound value before calculating next potential bound.

--- knapsack_branchandbound.py
+++ knapsack_branchandbound.py
@@ -20,7 +20,7 @@ class Node:
         self.allItems = items
         self.maxWeight = maxWeight
         self.taken = takenItems
-        self.bound = self.getValue()
+        self.bound = 0
 
     
     def getWeight(self):
@@ -38,6 +38,7 @@ class Node:
     def calcBound(self):
         # For all upcoming items calc it as a fractional
         cWeight = self.getWeight()
+        self.bound = self.getValue()
         item = None
         
         for i in range(self.level, len(self.allItems)):
@@ -75,7 +76,11 @@ def knapsackSolve(items, maxWeight):
     while len(tree) > 0:
         cNode = tree.pop(0)
         
+        print str(cNode.bound) + " --- " + str(bestNode.getValue())
+        print cNode.taken
         if cNode.bound > bestNode.getValue() and cNode.level < len(items):
+            print "+"
+            
             withNode = Node(cNode.level + 1, items, cNode.taken[:], maxWeight)
             itemToAdd = items[cNode.level]
         
@@ -87,11 +92,13 @@ def knapsackSolve(items, maxWeight):
                     bestNode = withNode
                     
                 if withNode.bound > bestNode.getValue():
+                    print "with " + itemToAdd.name + " (%d)" % (withNode.bound)
                     tree.append(withNode)
             
             withoutNode = Node(cNode.level + 1, items, cNode.taken[:], maxWeight)
             withoutNode.calcBound()
             if withoutNode.bound > bestNode.getValue():
+                print "without " + itemToAdd.name + " (%d)" % (withoutNode.bound)
                 tree.append(withoutNode)
     
     
@@ -99,4 +106,4 @@ def knapsackSolve(items, maxWeight):
         print item.name
 
     
-knapsackSolve([Item("1", 45, 3), Item("2", 30, 5), Item("3", 45, 9), Item("4", 10, 5)], 16)
+knapsackSolve([Item("A", 45, 3), Item("B", 30, 5), Item("C", 45, 9), Item("D", 10, 5)], 16)
