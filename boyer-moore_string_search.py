b865b8fb5096dc4afd64417d9c79b482c84973f9
Kirill <kirilodius@gmail.com>
2015-08-15 01:34:54 +0200

    Update boyer-moore_string_search.py
    Working version

--- boyer-moore_string_search.py
+++ boyer-moore_string_search.py
@@ -1,23 +1,41 @@
 def boyer_moore_search(pat, txt):
+    if len(pat) > len(txt) or min(len(pat), len(txt)) == 0:
+        return None
+    
     stops = bStops(pat)
-    print stops
+    prefixes = pref_func(pat)
+    rprefixes = pref_func(pat[::-1])
+    pref_table = []
+    for i in prefixes:
+        pref_table.append(prefixes[-1])
+    
+    for i in range(1, len(pat)):
+        j = rprefixes[i]
+        pref_table[j] = min(pref_table[j], i - j + 1)
+    
+    txt_offset = len(pat) - 1
+
+    while txt_offset < len(txt):
+        pat_offset = len(pat) - 1
+        kof = txt_offset
+        while txt[kof] == pat[pat_offset]:
+            if pat_offset == 0:
+                return kof
+            kof -= 1
+            pat_offset -= 1
+
+        if (pat_offset == len(pat) - 1):
+            txt_offset += stops.get(txt[txt_offset], len(pat))
+        else:
+            txt_offset += pref_table[len(pat) - pat_offset - 1]
+    
+    return -1
     
 
 def bStops(pat):
-    return dict([(l, pat[:-1].rfind(l) + 1) for l in pat[:-1]])
+    return dict([(l, len(pat) - pat.rfind(l) - 1) for l in pat[:-1]])
     
-def bSuffix(pat):
-    pi = pref_func(pat)
-    pi1 = pref_func(pat[::-1])
-    suffshift = [0 for i in range(len(pat))]
-    for i in range(len(pat)):
-        suffshift[i] = len(pat) - pi[len(pat) - 1]
 
-    for i in range(1, len(pat)):
-        idx = len(pat) - 1 - pi1[i]
-        suffshift[i] = min(suffshift[idx], i - pi1[i])
-    
-    return suffshift
     
 def pref_func(pat):
     p = [0 for i in range(len(pat))]
@@ -30,6 +48,5 @@ def pref_func(pat):
         p[i] = k
     
     return p
-
-boyer_moore_search("abc", "abc")
-bSuffix("колокол")
+                                                           
+print(boyer_moore_search("abcabc", "abcabdabcabc"))
