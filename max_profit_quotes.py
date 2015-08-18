7bc0c9cd6cce1e4155b83e1788cefd18000aa731
Kirill <kirilodius@gmail.com>
2015-08-18 21:13:53 +0200

    Update max_profit_quotes.py

--- max_profit_quotes.py
+++ max_profit_quotes.py
@@ -7,3 +7,23 @@ def maxProfit(quotes):
         max_profit = max(profit_from_next, max_profit)
         
 maxProfit([310, 315, 275, 295, 260, 270, 290, 230, 255, 250])
+
+
+def maxLength(arr):
+    max_length = 0
+    p_i = arr[0]
+    
+    length = 0
+    for i in arr[1:]:
+        if i == p_i:
+            length += 1
+        else:
+            length = 1
+        max_length = max(length, max_length)
+        
+        p_i = i
+    
+    return max_length
+        
+        
+maxProfit([310, 315, 275, 275, 275, 270, 290, 230, 255, 250])
